72efb48637b43fb990747becd9359e01bd4f7a3e
Ludovic Cleroux <ludydoo@gmail.com`>
2022-10-19 13:27:10 +0200

    COR-508 Fix Terraform Manifests

--- deploy/terraform/azurerm_linux_web_app.app.tf
+++ deploy/terraform/azurerm_linux_web_app.app.tf
@@ -24,26 +24,19 @@ resource "azurerm_linux_web_app" "app" {
   }
   app_settings = {
     # See https://learn.microsoft.com/en-us/azure/app-service/configure-custom-container?pivots=container-linux#configure-port-number
-    WEBSITES_PORT = var.port
-    oidc_AUTHENTICATION_SECRET = var.oidc_client_secret
-    CORE_DB_DSN = "postgres://${random_pet.postgres_admin_username.id}:${random_password.postgres_admin_password.result}@${azurerm_postgresql_flexible_server.postgres.fqdn}/${azurerm_postgresql_flexible_server_database.db.name}?sslmode=require"
-    CORE_DB_DRIVER = "postgres"
-    CORE_LISTEN_ADDRESS = ":${var.port}"
+    WEBSITES_PORT               = var.port
+    oidc_AUTHENTICATION_SECRET  = var.oidc_client_secret
+    CORE_DB_DSN                 = "postgres://${random_pet.postgres_admin_username.id}:${random_password.postgres_admin_password.result}@${azurerm_postgresql_flexible_server.postgres.fqdn}/${azurerm_postgresql_flexible_server_database.db.name}?sslmode=require"
+    CORE_DB_DRIVER              = "postgres"
+    CORE_LISTEN_ADDRESS         = ":${var.port}"
     # See https://learn.microsoft.com/en-us/azure/app-service/configure-authentication-customize-sign-in-out?source=recommendations#sign-out-of-a-session
-    CORE_LOGOUT_URL = "/.auth/logout"
+    CORE_LOGOUT_URL             = "/.auth/logout"
     CORE_JWT_GLOBAL_ADMIN_GROUP = var.jwt_global_admin_group
-    CORE_ID_TOKEN_HEADER_NAME = "x-ms-token-oidc-id-token"
+    CORE_ID_TOKEN_HEADER_NAME   = "x-ms-token-oidc-id-token"
   }
   sticky_settings {
     app_setting_names = [
       "oidc_AUTHENTICATION_SECRET",
-    ]
-  }
-  lifecycle {
-    ignore_changes = [
-      site_config.0.application_stack.0.docker_image,
-      site_config.0.application_stack.0.docker_image_tag,
-      app_settings["DOCKER_CUSTOM_IMAGE_NAME"],
     ]
   }
   depends_on = [
