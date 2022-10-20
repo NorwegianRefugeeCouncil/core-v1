7028e6d9dd9bcc336946f2b4a8ba8f84c7862e87
Ludovic Cleroux <ludydoo@gmail.com>
2022-10-20 13:22:08 +0200

    Cor 502 Fix individual upload country_id (#20)
    COR-502 set country on individual upload

--- internal/handlers/individuals_upload.go
+++ internal/handlers/individuals_upload.go
@@ -61,6 +61,18 @@ func UploadHandler(individualRepo db.IndividualRepo) http.Handler {
 			return
 		}
 
+		selectedCountryID, err := utils.GetSelectedCountryID(ctx)
+		if err != nil {
+			l.Error("failed to get selected country id", zap.Error(err))
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		for _, individual := range individuals {
+			individual.CountryID = selectedCountryID
+			fields = append(fields, "country_id")
+		}
+
 		for _, individual := range individuals {
 			if !authIntf.CanReadWriteToCountryID(individual.CountryID) {
 				l.Warn("user does not have permission to upload individuals to country", zap.String("country_id", individual.CountryID))
