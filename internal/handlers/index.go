20995d62e82a171b4be490087b5f72571b3d5d97
Irena Laemmerer <irena.lammerer@nrc.no>
2022-10-20 09:01:39 +0200

    COR-510: renaming

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func HandleIndex(templates map[string]*template.Template) http.Handler {
+func HandleHome(templates map[string]*template.Template) http.Handler {
 	const templateName = "home.gohtml"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		renderView(templates, templateName, w, r, viewParams{})
