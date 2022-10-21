23606bed7979d90047c3f7af7d9fcc0ffbf1014a
Irena Laemmerer <irena.lammerer@nrc.no>
2022-10-21 16:21:13 +0200

    COR-519: UX fixes

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -6,7 +6,7 @@ import (
 )
 
 func HandleHome(templates map[string]*template.Template) http.Handler {
-	const templateName = "home.gohtml"
+	const templateName = "individuals.gohtml"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		renderView(templates, templateName, w, r, viewParams{})
 	})
