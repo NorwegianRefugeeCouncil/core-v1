d2090442f559c9c9815f4dcec2c7dc10dd85f2d1
Ben McAlindin <bmcalindin@gmail.com>
2022-10-24 13:12:38 +0200

    Redirect on index load

--- internal/handlers/index.go
+++ internal/handlers/index.go
@@ -1,13 +1,35 @@
 package handlers
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
+
+	"github.com/nrc-no/notcore/internal/logging"
+	"github.com/nrc-no/notcore/internal/utils"
+	"go.uber.org/zap"
 )
 
 func HandleHome(templates map[string]*template.Template) http.Handler {
-	const templateName = "individuals.gohtml"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		renderView(templates, templateName, w, r, viewParams{})
+		var (
+			ctx = r.Context()
+			l   = logging.NewLogger(ctx)
+		)
+
+		authInterface, err := utils.GetAuthContext(ctx)
+		if err != nil {
+			l.Error("failed to get auth context", zap.Error(err))
+			http.Error(w, "couldn't get auth context: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		allowedCountryIDs := authInterface.GetCountryIDsWithReadWritePermissions().Items()
+
+		if authInterface.IsGlobalAdmin() || len(allowedCountryIDs) > 1 {
+			http.Redirect(w, r, "/countries", http.StatusTemporaryRedirect)
+		} else {
+			http.Redirect(w, r, fmt.Sprintf("/countries/%s/individuals", allowedCountryIDs[0]), http.StatusTemporaryRedirect)
+		}
 	})
 }
