abeee4e922dfeda76a4b29b0ebb8d32a9da58751
Ben McAlindin <bmcalindin@gmail.com>
2022-10-24 12:59:11 +0200

    Take country id from path param

--- internal/server/middleware_selected_country.go
+++ internal/server/middleware_selected_country.go
@@ -2,8 +2,8 @@ package server
 
 import (
 	"net/http"
-	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/nrc-no/notcore/internal/logging"
 	"github.com/nrc-no/notcore/internal/utils"
 	"go.uber.org/zap"
@@ -12,22 +12,16 @@ import (
 func selectedCountryMiddleware() func(handler http.Handler) http.Handler {
 	return func(h http.Handler) http.Handler {
 
-		const cookieName = "core_selectedCountryID"
+		const pathParamCountryID = "country_id"
 
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			ctx := r.Context()
 			l := logging.NewLogger(ctx)
 
-			l.Debug("selected country middleware")
-			selectedCountryIDCookie, err := r.Cookie(cookieName)
-			if err != nil {
-				l.Debug("selected country cookie not found")
-				h.ServeHTTP(w, r)
-				return
-			}
+			countryId := mux.Vars(r)[pathParamCountryID]
 
-			l.Debug("selected country cookie value", zap.String("cookie", selectedCountryIDCookie.Value))
+			l.Debug("selected country id path param", zap.String("country_id", countryId))
 			allCountries, err := utils.GetCountries(ctx)
 			if err != nil {
 				l.Error("failed to get all countries", zap.Error(err))
@@ -38,7 +32,7 @@ func selectedCountryMiddleware() func(handler http.Handler) http.Handler {
 			// checking if the selected country id is actually a valid country id
 			found := false
 			for _, c := range allCountries {
-				if c.ID == selectedCountryIDCookie.Value {
+				if c.ID == countryId {
 					found = true
 					break
 				}
@@ -46,17 +40,7 @@ func selectedCountryMiddleware() func(handler http.Handler) http.Handler {
 
 			if found {
 				// Store the selected country ID in the request context
-				r = r.WithContext(utils.WithSelectedCountryID(ctx, selectedCountryIDCookie.Value))
-			} else {
-				// Remove the cookie
-				http.SetCookie(w, &http.Cookie{
-					Name:     cookieName,
-					Value:    "",
-					Path:     "/",
-					Expires:  time.Unix(0, 0),
-					HttpOnly: true,
-					Secure:   true,
-				})
+				r = r.WithContext(utils.WithSelectedCountryID(ctx, countryId))
 			}
 			h.ServeHTTP(w, r)
 		})
