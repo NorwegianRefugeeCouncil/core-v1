ad3d920cc388ae785f04efa39ec29f9a7da81a41
Ben McAlindin <bmcalindin@gmail.com>
2022-10-26 10:19:43 +0200

    Remove authz from delete endpoint

--- internal/handlers/individual_delete.go
+++ internal/handlers/individual_delete.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/nrc-no/notcore/internal/db"
 	"github.com/nrc-no/notcore/internal/logging"
-	"github.com/nrc-no/notcore/internal/utils"
 	"go.uber.org/zap"
 )
 
@@ -24,23 +23,10 @@ func HandleIndividualDelete(repo db.IndividualRepo) http.Handler {
 			l   = logging.NewLogger(ctx)
 		)
 
-		authInterface, err := utils.GetAuthContext(ctx)
-		if err != nil {
-			l.Error("failed to get auth context", zap.Error(err))
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
 		individual, err := repo.GetByID(ctx, mux.Vars(r)[pathParamIndividualID])
 		if err != nil {
 			l.Error("failed to get individual", zap.Error(err))
 			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		if !authInterface.CanReadWriteToCountryID(individual.CountryID) {
-			l.Warn("user does not have permission to read/write to country", zap.String("country_id", individual.CountryID))
-			http.Error(w, "forbidden", http.StatusForbidden)
 			return
 		}
 
