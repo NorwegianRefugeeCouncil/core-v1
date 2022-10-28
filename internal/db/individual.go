b0744d84d81d39e378f1240e40229bf06b6358a2
Ben McAlindin <bmcalindin@gmail.com>
2022-10-28 13:40:02 +0200

    Add repo method to soft delete many individuals

--- internal/db/individual.go
+++ internal/db/individual.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
 	"github.com/nrc-no/notcore/internal/api"
 	"github.com/nrc-no/notcore/internal/containers"
 	"github.com/nrc-no/notcore/internal/logging"
@@ -22,6 +23,7 @@ type IndividualRepo interface {
 	Put(ctx context.Context, individual *api.Individual, fields []string) (*api.Individual, error)
 	PutMany(ctx context.Context, individuals []*api.Individual, fields []string) ([]*api.Individual, error)
 	SoftDelete(ctx context.Context, id string) error
+	SoftDeleteMany(ctx context.Context, ids []string) error
 }
 
 type individualRepo struct {
@@ -317,6 +319,29 @@ func (i individualRepo) softDeleteInternal(ctx context.Context, tx *sqlx.Tx, id 
 		l.Error("failed to delete individual", zap.Error(err))
 		return err
 	}
+	return nil
+}
+
+func (i individualRepo) SoftDeleteMany(ctx context.Context, ids []string) error {
+	_, err := doInTransaction(ctx, i.db, func(ctx context.Context, tx *sqlx.Tx) (interface{}, error) {
+		err := i.softDeleteManyInternal(ctx, tx, ids)
+		return nil, err
+	})
+	return err
+}
+
+func (i individualRepo) softDeleteManyInternal(ctx context.Context, tx *sqlx.Tx, ids []string) error {
+	l := logging.NewLogger(ctx).With(zap.Strings("individual_ids", ids))
+	l.Debug("deleting individuals")
+
+	const query = "UPDATE individuals SET deleted_at = $1 WHERE id = ANY($2) and deleted_at IS NULL"
+	var args = []interface{}{time.Now().UTC(), pq.Array(ids)}
+
+	if _, err := tx.ExecContext(ctx, query, args...); err != nil {
+		l.Error("failed to delete individuals", zap.Error(err))
+		return err
+	}
+
 	return nil
 }
 
