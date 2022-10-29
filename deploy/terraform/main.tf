57e6d670de6fbf00aaef4a9880f5733a6f908ecd
Ludovic Cleroux <ludydoo@gmail.com`>
2022-10-29 06:52:04 +0200

    Commit terraform lock

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -2,11 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.27.0"
+      version = "~> 3.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "1.0.0"
+      version = "~> 1.0"
+    }
+    random = {
+        source  = "hashicorp/random"
+        version = "~> 3.0"
     }
   }
   backend "azurerm" {
