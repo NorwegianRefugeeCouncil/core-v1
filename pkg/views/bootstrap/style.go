00f25990a20c6285afa7cead40c6c55d6ec6933a
Ludovic Cleroux <ludydoo@gmail.com>
2022-11-11 15:20:24 +0100

    COR-653 add form alerts (#105)

---
+++ pkg/views/bootstrap/style.go
@@ -0,0 +1,11 @@
+package bootstrap
+
+type Style string
+
+const (
+	StyleSuccess Style = "success"
+	StyleDanger  Style = "danger"
+	StyleInfo    Style = "info"
+	StyleWarning Style = "warning"
+	StylePrimary Style = "primary"
+)
