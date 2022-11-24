bc506a2794c1d415442017267b1d258bc7f074a1
Ben McAlindin <bmcalindin@gmail.com>
2022-11-24 17:18:05 +0100

    Remove validation

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -289,10 +289,6 @@ variable "service_plan_sku_name" {
   type        = string
   default     = "P1v2"
   description = <<EOF
-THe sku name of the app service plan.
+THe sku name of the app service plan. https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan#sku_name
 EOF
-  validation {
-    condition     = contains(["B1", "B2", "B3", "D1", "F1", "I1", "I2", "I3", "I1v2", "I2v2", "I3v2", "P1v2", "P2v2", "P3v2", "P1v3", "P2v3", "P3v3", "S1", "S2", "S3", "SHARED", "EP1", "EP2", "EP3", "WS1", "WS2", "WS3", "Y1"], var.service_plan_sku_name)
-    error_message = "The sku name must be one of https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/service_plan#sku_name."
-  }
 }
