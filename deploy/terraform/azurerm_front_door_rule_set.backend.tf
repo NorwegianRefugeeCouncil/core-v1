85a849118a2115a4397185ab8ad053a5ae385033
Ludovic Cleroux <ludydoo@gmail.com>
2022-12-01 19:56:01 +0100

    fix rule set name

--- deploy/terraform/azurerm_front_door_rule_set.backend.tf
+++ deploy/terraform/azurerm_front_door_rule_set.backend.tf
@@ -1,5 +1,5 @@
 resource "azurerm_cdn_frontdoor_rule_set" "backend" {
   provider                 = azurerm.runtime
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
-  name                     = "rules"
+  name                     = "backend"
 }
