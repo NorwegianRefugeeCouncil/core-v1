9bce9152ebd90ff92002045bee3b9173cd75b825
Irena Laemmerer <irena.lammerer@nrc.no>
2022-12-01 17:23:19 +0100

    only drop column if exists

--- internal/db/migrations/postgres/018_add_nrc_organisation_to_country.up.sql
+++ internal/db/migrations/postgres/018_add_nrc_organisation_to_country.up.sql
@@ -1,6 +1,7 @@
 ALTER TABLE countries
     -- drop jwt_group column
-    DROP COLUMN jwt_group;
+    DROP COLUMN IF EXISTS jwt_group;
+
 ALTER TABLE countries
     -- add nrc_organisation column
     ADD COLUMN nrc_organisation      VARCHAR(255)         NOT NULL DEFAULT '';
