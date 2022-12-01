85e64caced8fb976a9b5138b9daea59d2509bf43
Ludovic Cleroux <ludydoo@gmail.com>
2022-12-01 17:56:57 +0100

    update frontdoor cache config

--- deploy/terraform/azurerm_cdn_frontdoor_route.fd.tf
+++ deploy/terraform/azurerm_cdn_frontdoor_route.fd.tf
@@ -4,7 +4,6 @@ resource "azurerm_cdn_frontdoor_route" "backend" {
   cdn_frontdoor_endpoint_id     = azurerm_cdn_frontdoor_endpoint.backend.id
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.backend.id
   cdn_frontdoor_origin_ids      = [azurerm_cdn_frontdoor_origin.backend.id]
-  // cdn_frontdoor_rule_set_ids    = [azurerm_cdn_frontdoor_rule_set.fd.id]
   enabled = true
 
   forwarding_protocol    = "HttpsOnly"
@@ -16,4 +15,50 @@ resource "azurerm_cdn_frontdoor_route" "backend" {
     azurerm_cdn_frontdoor_custom_domain.backend.id,
   ]
   link_to_default_domain = false
+  cache {
+    query_string_caching_behavior = "UseQueryString"
+    compression_enabled           = true
+    content_types_to_compress = [
+      "application/eot",
+      "application/font",
+      "application/font-sfnt",
+      "application/javascript",
+      "application/json",
+      "application/opentype",
+      "application/otf",
+      "application/pkcs7-mime",
+      "application/truetype",
+      "application/ttf",
+      "application/vnd.ms-fontobject",
+      "application/xhtml+xml",
+      "application/xml",
+      "application/xml+rss",
+      "application/x-font-opentype",
+      "application/x-font-truetype",
+      "application/x-font-ttf",
+      "application/x-httpd-cgi",
+      "application/x-mpegurl",
+      "application/x-otf",
+      "application/x-perl",
+      "application/x-ttf",
+      "application/x-javascript",
+      "font/eot",
+      "font/ttf",
+      "font/otf",
+      "font/opentype",
+      "image/svg+xml",
+      "text/css",
+      "text/csv",
+      "text/html",
+      "text/javascript",
+      "text/js",
+      "text/plain",
+      "text/richtext",
+      "text/tab-separated-values",
+      "text/xml",
+      "text/x-script",
+      "text/x-component",
+      "text/x-java-source"
+    ]
+  }
 }
---
+++ deploy/terraform/azurerm_front_door_rule.backend_disable_auth_cache.tf
@@ -0,0 +1,26 @@
+resource "azurerm_cdn_frontdoor_rule" "backend_disable_auth_cache" {
+  provider = azurerm.runtime
+  # Required as per terraform documentation
+  depends_on = [
+    azurerm_cdn_frontdoor_origin_group.backend,
+    azurerm_cdn_frontdoor_origin.backend,
+  ]
+
+  name                      = "disableAuthCache"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.backend.id
+  order                     = 1
+  behavior_on_match         = "Continue"
+
+  actions {
+    route_configuration_override_action {
+      cache_behavior                = "Disable"
+    }
+  }
+
+  conditions {
+    request_uri_condition {
+      operator = "BeginsWith"
+      match_values = ["/.auth"]
+    }
+  }
+}
