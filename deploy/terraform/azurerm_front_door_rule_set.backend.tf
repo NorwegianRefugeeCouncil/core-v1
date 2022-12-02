de2eb5aa8d1563498eb57da5717fe95f17414ad3
Ludovic Cleroux <ludydoo@gmail.com>
2022-12-02 07:51:11 +0100

    update frontdoor cache config (#139)

---
+++ deploy/terraform/azurerm_front_door_rule_set.backend.tf
@@ -0,0 +1,5 @@
+resource "azurerm_cdn_frontdoor_rule_set" "backend" {
+  provider                 = azurerm.runtime
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
+  name                     = "backend"
+}
