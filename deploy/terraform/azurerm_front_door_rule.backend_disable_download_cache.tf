12ab9f86f3d0917ee8238a12239e41e66ad71177
Ben McAlindin <ben.mcalindin@nrc.no>
2022-12-02 18:39:01 +0100

    Disable caching for download requests (#145)
    * Disable caching for download requests
    
    * Disable compression
    
    * Remove cache rule

---
+++ deploy/terraform/azurerm_front_door_rule.backend_disable_download_cache.tf
@@ -0,0 +1,26 @@
+resource "azurerm_cdn_frontdoor_rule" "backend_disable_download_compression" {
+  provider = azurerm.runtime
+  # Required as per terraform documentation
+  depends_on = [
+    azurerm_cdn_frontdoor_origin_group.backend,
+    azurerm_cdn_frontdoor_origin.backend,
+  ]
+
+  name                      = "disableDownloadCompression"
+  cdn_frontdoor_rule_set_id = azurerm_cdn_frontdoor_rule_set.backend.id
+  order                     = 1
+  behavior_on_match         = "Continue"
+
+  actions {
+    route_configuration_override_action {
+      compression_enabled           = false 
+    }
+  }
+
+  conditions {
+    request_uri_condition {
+      operator = "EndsWith"
+      match_values = ["/download"]
+    }
+  }
+}
