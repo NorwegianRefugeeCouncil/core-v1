7773298860bb84a6d17b2e2687df527557f6be03
Irena Laemmerer <irena.lammerer@nrc.no>
2023-03-09 16:58:44 +0100

    fix terraform provider versions

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.33"
+      version = "3.33"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 1.1"
+      version = "1.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.4"
+      version = "3.4.3"
     }
   }
   backend "azurerm" {
