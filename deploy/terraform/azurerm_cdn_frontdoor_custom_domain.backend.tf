c6a22506ebf136b955333cec59cdf4f2b8672a0a
Ben McAlindin <bmcalindin@gmail.com>
2023-06-12 15:32:25 +0200

    Fix

--- deploy/terraform/azurerm_cdn_frontdoor_custom_domain.backend.tf
+++ deploy/terraform/azurerm_cdn_frontdoor_custom_domain.backend.tf
@@ -2,7 +2,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "backend" {
   provider                 = azurerm.runtime
   name                     = "backend"
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.fd.id
-  dns_zone_id              = azurerm_dns_zone.dns.id
+  dns_zone_id              = data.azurerm_dns_zone.dns.id
   host_name                = var.backend_host_name
 
   tls {
@@ -13,7 +13,7 @@ resource "azurerm_cdn_frontdoor_custom_domain" "backend" {
 
 resource "azurerm_dns_txt_record" "backend" {
   name                = "_dnsauth.${var.environment}-${var.app_name}"
-  zone_name           = azurerm_dns_zone.dns.name
+  zone_name           = data.azurerm_dns_zone.dns.name
   resource_group_name = var.infra_resource_group_name
   ttl                 = 3600
 
@@ -26,7 +26,7 @@ resource "azurerm_dns_cname_record" "backend" {
   depends_on = [azurerm_cdn_frontdoor_route.backend]
 
   name                = "${var.environment}-${var.app_name}" 
-  zone_name           = azurerm_dns_zone.dns.name
+  zone_name           = data.azurerm_dns_zone.dns.name
   resource_group_name = var.infra_resource_group_name
   ttl                 = 3600
   record              = azurerm_cdn_frontdoor_endpoint.backend.host_name
