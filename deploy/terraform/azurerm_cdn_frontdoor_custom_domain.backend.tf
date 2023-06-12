3d969fb44e5784c9a3e4ced6a61980c10c26533c
Ben McAlindin <bmcalindin@gmail.com>
2023-06-12 14:42:07 +0200

    move things

--- deploy/terraform/azurerm_cdn_frontdoor_custom_domain.backend.tf
+++ deploy/terraform/azurerm_cdn_frontdoor_custom_domain.backend.tf
@@ -10,3 +10,24 @@ resource "azurerm_cdn_frontdoor_custom_domain" "backend" {
     minimum_tls_version = "TLS12"
   }
 }
+
+resource "azurerm_dns_txt_record" "backend" {
+  name                = "_dnsauth.${var.environment}-${var.app_name}"
+  zone_name           = azurerm_dns_zone.dns.name
+  resource_group_name = var.infra_resource_group_name
+  ttl                 = 3600
+
+  record {
+    value = azurerm_cdn_frontdoor_custom_domain.backend.validation_token
+  }
+}
+
+resource "azurerm_dns_cname_record" "backend" {
+  depends_on = [azurerm_cdn_frontdoor_route.backend]
+
+  name                = "${var.environment}-${var.app_name}" 
+  zone_name           = azurerm_dns_zone.dns.name
+  resource_group_name = var.infra_resource_group_name
+  ttl                 = 3600
+  record              = azurerm_cdn_frontdoor_endpoint.backend.host_name
+}
