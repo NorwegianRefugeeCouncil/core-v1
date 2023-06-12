1fa9fdab8747ec84869b66bf4b538255326c0059
Ben McAlindin <bmcalindin@gmail.com>
2023-06-12 14:28:56 +0200

    Make dns zone data

--- deploy/terraform/azurerm_dns_zone.dns.tf
+++ deploy/terraform/azurerm_dns_zone.dns.tf
@@ -1,4 +1,4 @@
-resource "azurerm_dns_zone" "dns" {
+data "azurerm_dns_zone" "dns" {
   provider            = azurerm.infra
   name                = var.infra_dns_zone_name
   resource_group_name = var.infra_resource_group_name
