02d4a370c05543d37462218af2e009f38c7d36d9
Irena Laemmerer <irena.lammerer@nrc.no>
2023-06-21 12:31:26 +0200

    add dns a record set

--- deploy/terraform/azurerm_dns_zone.dns.tf
+++ deploy/terraform/azurerm_dns_zone.dns.tf
@@ -3,3 +3,11 @@ resource "azurerm_dns_zone" "dns" {
   name                = var.dns_zone_name
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_dns_a_record" "dns_a_record" {
+  name                = "dns-a-record"
+  zone_name           = azurerm_dns_zone.dns.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 3600
+  target_resource_id  = azurerm_cdn_frontdoor_origin.backend.id
+}
