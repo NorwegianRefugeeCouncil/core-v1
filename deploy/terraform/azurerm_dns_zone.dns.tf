6071ff4433be59e13d7c93d10c8ba8fbad31f775
Irena Laemmerer <irena.lammerer@nrc.no>
2023-06-29 12:11:19 +0200

    increase app instances & remove dns records

--- deploy/terraform/azurerm_dns_zone.dns.tf
+++ deploy/terraform/azurerm_dns_zone.dns.tf
@@ -3,24 +3,3 @@ resource "azurerm_dns_zone" "dns" {
   name                = var.dns_zone_name
   resource_group_name = azurerm_resource_group.rg.name
 }
-
-resource "azurerm_dns_a_record" "dns_a_record" {
-  provider            = azurerm.runtime
-  name                = "@"
-  zone_name           = azurerm_dns_zone.dns.name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 3600
-  target_resource_id  = azurerm_cdn_frontdoor_endpoint.backend.id
-}
-
-resource "azurerm_dns_txt_record" "dns_txt_record" {
-  provider            = azurerm.runtime
-  name                = "_dnsauth"
-  zone_name           = azurerm_dns_zone.dns.name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 3600
-
-  record {
-    value = var.dns_text_record_value
-  }
-}
