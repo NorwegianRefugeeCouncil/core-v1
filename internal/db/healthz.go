0b9d41fdb0402fb977a6e6f6cffcf7ea0b83aed7
Ben McAlindin <bmcalindin@gmail.com>
2023-06-30 12:48:53 +0200

    Check the db when running health check

---
+++ internal/db/healthz.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"github.com/jmoiron/sqlx"
+	"github.com/nrc-no/notcore/internal/logging"
+	"go.uber.org/zap"
+)
+
+type HealthzRepo interface {
+	Check(ctx context.Context) (error)
+}
+
+type healthzRepo struct {
+	db *sqlx.DB
+}
+
+func NewHealthzRepo(db *sqlx.DB) HealthzRepo {
+	return &healthzRepo{db: db}
+}
+
+func (h healthzRepo) logger(ctx context.Context) *zap.Logger {
+	return logging.NewLogger(ctx)
+}
+
+func (h healthzRepo) Check(ctx context.Context) error {
+	l := h.logger(ctx)
+
+// check if the database is alive
+	if err := h.db.PingContext(ctx); err != nil {
+		l.Error("failed to ping database", zap.Error(err))
+		return err
+	}
+
+	return nil
+}
--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -2,10 +2,19 @@ package handlers
 
 import (
 	"net/http"
+
+	"github.com/nrc-no/notcore/internal/db"
 )
 
-func HandleHealth() http.Handler {
+func HandleHealth(healthzRepo db.HealthzRepo) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := healthzRepo.Check(r.Context())
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
 		w.WriteHeader(http.StatusOK)
 	})
 }
