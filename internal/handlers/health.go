bff3a4f01e29037c6072170b7b4c3bd7d7871bde
Ben McAlindin <bmcalindin@gmail.com>
2023-06-30 12:50:15 +0200

    log on successful health check

--- internal/handlers/health.go
+++ internal/handlers/health.go
@@ -4,16 +4,21 @@ import (
 	"net/http"
 
 	"github.com/nrc-no/notcore/internal/db"
+	"github.com/nrc-no/notcore/internal/logging"
 )
 
 func HandleHealth(healthzRepo db.HealthzRepo) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		l := logging.NewLogger(ctx)
+
 		err := healthzRepo.Check(r.Context())
 
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
+
+		l.Info("health check ok")
 
 		w.WriteHeader(http.StatusOK)
 	})
