f7d083f42218156e2c96d0c48cefe6432effd72d
David Figueroa <david.figueroa@nrc.no>
2024-01-16 17:32:45 +0100

    fix: reverted dns name change

--- deploy/terraform/azurerm_private_dns_zone.storage_dns.tf
+++ deploy/terraform/azurerm_private_dns_zone.storage_dns.tf
@@ -1,5 +1,5 @@
 resource "azurerm_private_dns_zone" "storage_dns" {
   provider            = azurerm.runtime
-  name                = "${var.app_name}-${var.environment}.blob.core.windows.net"
+  name                = "privatelink.blob.core.windows.net"
   resource_group_name = azurerm_resource_group.rg.name
 }
